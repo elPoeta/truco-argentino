b9cdbcdc809b5b776f76ffb44d051aaa4a55cd64
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-10-22 20:55:55 -0300

    create contex draw background image

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('#canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+export { canvas, ctx }
