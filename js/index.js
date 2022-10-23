c0fb45590b167f29a10118465d543335a3f6b3cb
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-10-23 15:57:31 -0300

    draw cards

--- js/index.js
+++ js/index.js
@@ -1,10 +1,39 @@
 /** @type {HTMLCanvasElement} */
 import { canvas, ctx } from "./app.js";
-const animate = timeStamp => {
+
+const animate = (timeStamp) => {
+  const originalWidth = deckImg.width;
+  const originalHeight = deckImg.height;
+  const frameX = 0;
+  const frameY = 0;
+  const deckW = originalWidth / 12;
+  const deckH = originalHeight / 5;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(boardImg, 0, 0, canvas.width, canvas.height);
-  requestAnimationFrame(animate);
+  ctx.drawImage(
+    deckImg,
+    frameX * deckW,
+    frameY * deckH,
+    deckW,
+    deckH,
+    10,
+    10,
+    deckW,
+    deckH
+  );
 
-}
+  ctx.drawImage(
+    deckImg,
+    2 * deckW,
+    2 * deckH,
+    deckW,
+    deckH,
+    deckW + 50,
+    10,
+    deckW,
+    deckH
+  );
+  requestAnimationFrame(animate);
+};
 
 animate(0);
